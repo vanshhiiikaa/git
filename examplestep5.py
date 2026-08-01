0fe42b517bee6c9182aa293c1e9eb4ac94d16c55
Vanshika <vanshhiiikaa.in>
2026-08-01 23:16:35 +0530

    added filtering functionality

--- examplestep5.py
+++ examplestep5.py
@@ -12,3 +12,15 @@ print("Sample Data Frame is:")
 print(df)   
 print("Names (Single column returned as Series) of the DataFrame:")
 print(df["Name"])  # returns a single column as Series
+
+high = df[df["Salary"] > 60000]  # returns rows where Salary is greater than 60000
+print("Rows where Salary is greater than 60000:")
+print(high)
+
+#Filtering rows based on multiple conditions
+filtered = df[(df["Age"] > 25) & (df["Salary"] > 50000)]  # returns rows where Age is greater than 25 and Salary is greater than 50000
+print("Rows where Age is greater than 25 and Salary is greater than 50000:")            
+
+#using or condition
+filtered_or = df[(df["Age"] > 30) | (df["Salary"]> 60000)]  # returns rows where Age is greater than 30 or Salary is greater than 60000      
+print("Rows where Age is greater than 30 or Salary is greater than 60000:")
